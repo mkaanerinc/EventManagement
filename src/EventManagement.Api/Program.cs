beb376723f8cefb2341886f576bbd8e1de5f598d
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2025-04-12 17:06:13 +0300

    feat(api, infrastructure): Add persistence services and repository registrations
    - Added `EventManagement` connection string in the appsettings for database connection.
    - Implemented `AddPersistenceServices` extension method to register DbContext and repositories.
    - Configured `BaseDbContext` to use SQL Server with connection string from configuration.
    - Registered repositories: `IAttendeeRepository`, `IEventRepository`, `IReportRepository`, and `ITicketRepository`.
    - Integrated persistence services in application with `AddPersistenceServices` call.

--- src/EventManagement.Api/Program.cs
+++ src/EventManagement.Api/Program.cs
@@ -1,8 +1,11 @@
+using EventManagement.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ src/EventManagement.Infrastructure/Persistence/PersistenceServiceRegistiration.cs
@@ -0,0 +1,31 @@
+﻿using EventManagement.Application.Services.Repositories;
+using EventManagement.Infrastructure.Persistence.Contexts;
+using EventManagement.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Infrastructure.Persistence;
+
+public static class PersistenceServiceRegistiration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("EventManagement"));
+        });
+
+        services.AddScoped<IAttendeeRepository,AttendeeRepository>();
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<IReportRepository, ReportRepository>();
+        services.AddScoped<ITicketRepository, TicketRepository>();
+
+        return services;
+    }
+}
