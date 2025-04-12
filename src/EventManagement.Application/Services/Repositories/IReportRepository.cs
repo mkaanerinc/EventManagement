c881f62949f28736db7c7ce6d94420ca03246990
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2025-04-12 13:08:35 +0300

    feat(application): Add repository interfaces for domain entities.
    - Created interface definitions under `Services.Repositories` .
    - Prepared base structure for dependency injection and abstraction.

---
+++ src/EventManagement.Application/Services/Repositories/IReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Infrastructure.Persistence.Repositories;
+using EventManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Application.Services.Repositories;
+
+public interface IReportRepository : IAsyncRepository<Report,Guid>, IRepository<Report, Guid>
+{
+}
