dd9ece6fcde2fcb6d3095d412af54cc898864909
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2025-04-12 13:28:01 +0300

    feat(infrastructure): Add BaseDbContext for managing database access
    - Created `BaseDbContext` under `Persistence.Contexts`.
    - Defined DbSets for Event, Ticket, Attendee, and Report entities .
    - Applied configurations dynamically via `OnModelCreating`.
    - Included XML documentation for improved readability.

---
+++ src/EventManagement.Infrastructure/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,64 @@
+﻿using EventManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Infrastructure.Persistence.Contexts;
+
+/// <summary>
+/// Represents the base DbContext for the Event Management application.
+/// Manages database access and entity configurations.
+/// </summary>
+public class BaseDbContext : DbContext
+{
+    /// <summary>
+    /// Gets or sets the application configuration.
+    /// Used for accessing configuration values such as connection strings.
+    /// </summary>
+    protected IConfiguration Configuration { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Events DbSet.
+    /// </summary>
+    public DbSet<Event> Events { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Tickets DbSet.
+    /// </summary>
+    public DbSet<Ticket> Tickets { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Attendees DbSet.
+    /// </summary>
+    public DbSet<Attendee> Attendees { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Reports DbSet.
+    /// </summary>
+    public DbSet<Report> Reports { get; set; }
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseDbContext"/> class.
+    /// </summary>
+    /// <param name="options">The options to be used by the DbContext.</param>
+    /// <param name="configuration">The application configuration object.</param>
+    public BaseDbContext(DbContextOptions<BaseDbContext> options, IConfiguration configuration) : base(options)
+    {
+        Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Configures the entity mappings using configurations from the current assembly.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder used to configure entities.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
